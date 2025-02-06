994f99a787a89bca5ca833c5e3e7f48d911825b4
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-02-06 11:45:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function firstWord(str) {
-    return str.trim().split(' ')[0] || '';
+    let trimmed = str.trim();
+    if (trimmed === '') {
+        return '';
+    } else {
+        let words = trimmed.split(' ');
+        return words[0];
+    }
 }
-Do not change the code below
+
+// Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
