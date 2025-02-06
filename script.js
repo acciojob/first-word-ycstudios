7b12d2a0ace4da6a38de1117d1ef2ecb88aa138f
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-02-06 11:43:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+    return str.trim().split(' ')[0] || '';
 }
-
-// Do not change the code below
+Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
